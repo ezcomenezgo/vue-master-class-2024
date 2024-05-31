503e181287328282905405a7ef2c851d9376f514
Claire Li <s3310507057@gmail.com>
2024-05-31 19:57:58 +0200

    feat: add init pages and routes

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Home page</h1>
+    <router-link to="/projects">Go to projects page</router-link>
+  </div>
+</template>
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Projects page</h1>
+    <router-link to="/">Go to home page</router-link>
+  </div>
+</template>
